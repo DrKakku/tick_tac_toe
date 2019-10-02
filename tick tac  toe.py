093d8093f6e323596cb76ba140ae2693d9040bbf
Yash Tripathi <53153448+DrKakku@users.noreply.github.com>
2019-10-02 19:57:35 +0530

    Add files via upload

---
+++ tick tac  toe.py
@@ -0,0 +1,11 @@
+from os import system
+from time import sleep
+from display import display
+from display import print_disp
+system('cls')
+x = display()
+print_disp(x)
+sleep(2)
+system('cls')
+print_disp(x)
+input()
