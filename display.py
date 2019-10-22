c95112e400f0a5777d9a1e70946129745ff6779e
Yash Tripathi <53153448+DrKakku@users.noreply.github.com>
2019-10-23 01:17:45 +0530

    Total overhaul update
    This update brings new library elements to play with as i have created easu to use input and display functions which can easily take input and are similar to lego blocks as all the heavy work is doneby the functions and you just have to call them to use it
    
    all that is left is simple evel design (HOPEFULLY)
    
    Note - I have not tested the components working together yet and have ony tested them indivusialy

--- display.py
+++ display.py
@@ -1,13 +1,39 @@
+#This function creates a ticktacktoe matrice for you so you do not have to go through the hassle
 def display() :
-    l1 = ['   ','|','   ','|','   ']
-    l2 = ['=','=','=','=','=','=','=''=','=','=','=']
-    l3 = ['   ','|','   ','|','   ']
-    l4 = ['=','=','=','=','=','=','=''=','=','=','=']
-    l5 = ['   ','|','   ','|','   ']
+    d11 = d12 = d13 = d21 = d22 = d23 = d31 = d32 = d33 = '   '
+    l1 = [d11,'|',d12,'|',d13]
+    l2 = ['=''=''=''=','=''=''=''=','=''=''=']
+    l3 = [d21,'|',d22,'|',d23]
+    l4 = ['=''=''=''=','=''=''=''=','=''=''=']
+    l5 = [d31,'|',d32,'|',d33]
     display = [l1,l2,l3,l4,l5]
     return display
+#This function prints the matrice so that the " " do not interfere and we can have a clean matrice
 def print_disp (x):
     for i in x:
         for j in i:
-             print(j,end=f'')
+             print(j, end = '')
         print()
+#This code shoud replace the element of the table to the desired element z to the desired position (coordinates) to the table disp in a tic tac toe matrix
+def play (cordinates,z,disp):
+     x = y = int()
+     cordinates[0] = x
+     cordinates[1] = y
+     if x == 2:
+          x = x + 1
+     elif y == 2:
+          y = y + 1
+     elif y == 3:
+          y = y + 2               
+     elif x == 3:
+          x = x + 2
+     disp[x][y] = z
+     return disp
+def inp ():
+     raw_input = input ("Player 1 what do you want to play input in x , y location")
+     cordinate = []    
+     list_input = raw_input.split(",",1)
+     for i in list_input:
+           i = int(i)
+           cordinate.append(i)
+     return cordinate
--- tick tac  toe.py
+++ tick tac  toe.py
@@ -1,11 +1,8 @@
 from os import system
-from time import sleep
-from display import display
-from display import print_disp
-system('cls')
-x = display()
-print_disp(x)
-sleep(2)
-system('cls')
-print_disp(x)
-input()
+from display import *
+print_disp(display())
+x = display() 
+play(inp(),"#",x)
+play(inp(),"$",x)
+play(inp(),"#",x)
+play(inp(),"$",x)
