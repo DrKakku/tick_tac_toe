b14642c420aa139948b994478ea92b95d0850275
Yash Tripathi <53153448+DrKakku@users.noreply.github.com>
2019-10-23 22:43:00 +0530

    basic work done
    The code works now and you can play with  your friend and many bugs are removed but a final patch is yet to made
    expect inprovments
    bugs fixeing
    main menu of sorts
    single player(no promises)

--- display.py
+++ display.py
@@ -10,30 +10,50 @@ def display() :
     return display
 #This function prints the matrice so that the " " do not interfere and we can have a clean matrice
 def print_disp (x):
-    for i in x:
+
+     for i in x:
         for j in i:
-             print(j, end = '')
+             print (j , end = '')
         print()
+     return x
 #This code shoud replace the element of the table to the desired element z to the desired position (coordinates) to the table disp in a tic tac toe matrix
-def play (cordinates,z,disp):
+def play (z,disp):
+#This code will take input and convert it into a list of single cordinate which is seprated by " " the cordinates are intiger in nature so can be used in spliting   
+#__________________________________________________________________________________________________________________#
+     cordinates = []  
+     num = ['1','2','3']
+     conditin = False
+     while conditin == False: 
+          raw_input = input ("what do you want to play input in x(row) y(column) location :- ") 
+          if raw_input[0] in num and raw_input[2] in num:
+            conditin = True
+            list_input = raw_input.split(" ")
+            for i in list_input:
+                 i = int(i)
+                 cordinates.append(i)
+          else :
+              print ("please enter a numeric value ")          
+
+#______________________________________________________________________________________________________________________#
+
+
+
      x = y = int()
-     cordinates[0] = x
-     cordinates[1] = y
-     if x == 2:
+     x = cordinates[0]
+     y = cordinates[1]
+     global counter
+     if x == 1:
+          x = 0
+     if y == 1:
+          y = 0
+     if y == 3:
+          y = y + 1               
+     if x == 3:
           x = x + 1
-     elif y == 2:
-          y = y + 1
-     elif y == 3:
-          y = y + 2               
-     elif x == 3:
-          x = x + 2
+     if disp[x][y] != '   ':
+          print ("Acting smart are we try to play on another position ")
+          disp[4][4] = '    '
+          return disp
+          
      disp[x][y] = z
      return disp
-def inp ():
-     raw_input = input ("Player 1 what do you want to play input in x , y location")
-     cordinate = []    
-     list_input = raw_input.split(",",1)
-     for i in list_input:
-           i = int(i)
-           cordinate.append(i)
-     return cordinate
--- tick tac  toe.py
+++ tick tac  toe.py
@@ -1,8 +1,27 @@
-from os import system
 from display import *
 print_disp(display())
 x = display() 
-play(inp(),"#",x)
-play(inp(),"$",x)
-play(inp(),"#",x)
-play(inp(),"$",x)
+global counter
+counter = 0
+win = False
+while win == False:
+    counter = counter + 1
+    print(counter)
+    if counter % 2 > 0:
+        print("Player 1 :")
+        print_disp(play(" X ",x))
+        if x[4][4] == '    ':
+            counter = counter + 1
+            x[4][4] = '   '
+        if x[0][0] == x[0][2] == x[0][4] != '   ' or x[2][0] == x[2][2] == x[2][4] != '   ' or x[4][0] == x[4][2] == x[4][4] != '   ' or x[0][0] == x[2][0] == x[4][0] != '   ' or x[0][2] == x[2][2] == x[4][2] != '   ' or x[0][4] == x[2][4] == x[4][4] != '   ' or x[0][0] == x[2][2] == x[4][4] != '   ' or x[0][4] == x[2][2] == x[4][0] != '   ' :
+            print("congulation PLAYER 1 won ")
+            win = True
+    elif counter % 2 == 0:
+        print("Player 2 :")
+        print_disp(play(" O ",x))
+        if x[4][4] == '    ':
+            counter = counter + 1
+            x[4][4] = '   '        
+        if x[0][0] == x[0][2] == x[0][4] != '   ' or x[2][0] == x[2][2] == x[2][4] != '   ' or x[4][0] == x[4][2] == x[4][4] != '   ' or x[0][0] == x[2][0] == x[4][0] != '   ' or x[0][2] == x[2][2] == x[4][2] != '   ' or x[0][4] == x[2][4] == x[4][4] != '   ' or x[0][0] == x[2][2] == x[4][4] != '   ' or x[0][4] == x[2][2] == x[4][0] != '   ' :
+            print("congulation PLAYER 2 won ")
+            win = True
